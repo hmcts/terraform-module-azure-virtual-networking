8394f68e9f0c20b336d195beda668585fee20ff5
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-09-17 11:52:33 +0100

    Update bgp_route_propagation

--- routing.tf
+++ routing.tf
@@ -3,7 +3,7 @@ resource "azurerm_route_table" "this" {
   name                          = each.value.name_override == null ? "${local.name}-${each.key}-${var.env}" : each.value.name_override
   resource_group_name           = each.value.resource_group_override == null ? local.resource_group : each.value.resource_group_override
   location                      = var.location
-  disable_bgp_route_propagation = false
+  bgp_route_propagation_enabled = true
   tags                          = var.common_tags
 }
 
