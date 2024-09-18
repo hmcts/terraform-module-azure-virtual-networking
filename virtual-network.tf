e871dbb8df4c342495911af219ee2a3a6cab594f
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-09-18 09:03:27 +0100

    Add private_endpoint_network_policies

--- virtual-network.tf
+++ virtual-network.tf
@@ -8,12 +8,13 @@ resource "azurerm_virtual_network" "this" {
 }
 
 resource "azurerm_subnet" "this" {
-  for_each             = { for subnet in local.flattened_subnets : "${subnet.vnet_key}-${subnet.subnet_key}" => subnet }
-  name                 = each.value.subnet.name_override == null ? "${local.name}-${each.value.subnet_key}-${var.env}" : each.value.subnet.name_override
-  resource_group_name  = local.resource_group
-  virtual_network_name = each.value.vnet.existing ? each.value.vnet_key : azurerm_virtual_network.this[each.value.vnet_key].name
-  address_prefixes     = each.value.subnet.address_prefixes
-  service_endpoints    = each.value.subnet.service_endpoints
+  for_each                          = { for subnet in local.flattened_subnets : "${subnet.vnet_key}-${subnet.subnet_key}" => subnet }
+  name                              = each.value.subnet.name_override == null ? "${local.name}-${each.value.subnet_key}-${var.env}" : each.value.subnet.name_override
+  resource_group_name               = local.resource_group
+  virtual_network_name              = each.value.vnet.existing ? each.value.vnet_key : azurerm_virtual_network.this[each.value.vnet_key].name
+  address_prefixes                  = each.value.subnet.address_prefixes
+  service_endpoints                 = each.value.subnet.service_endpoints
+  private_endpoint_network_policies = each.value.private_endpoint_network_policies
 
   dynamic "delegation" {
     for_each = each.value.subnet.delegations != null ? each.value.subnet.delegations : {}
